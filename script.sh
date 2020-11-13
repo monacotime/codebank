776aa9a1ac49f62e57e574302674fb1774000aee
Rudramani Singha <monacotime@outlook.com>
2020-11-13 12:02:38 +0530

    test post final

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 dos2unix tools/build.sh  
 dos2unix tools/bump-version.sh  
-dos2unix tools/deploy.sh  init.sh  
+dos2unix tools/deploy.sh
 dos2unix tools/pv.sh  
 dos2unix tools/run.sh  
 dos2unix tools/test.sh
@@ -9,13 +9,3 @@ dos2unix tools/init.sh
 dos2unix _scripts/sh/create_pages.sh  
 dos2unix _scripts/sh/dump_lastmod.sh  
 dos2unix _scripts/sh/sync_monitor.sh
-dos2unix .container/tools/build.sh 
-dos2unix .container/tools/bump-version.sh 
-dos2unix .container/tools/deploy.sh  init.sh 
-dos2unix .container/tools/pv.sh
-dos2unix .container/tools/run.sh  
-dos2unix .container/tools/test.sh
-dos2unix .container/tools/tools/init.sh
-dos2unix .container/_scripts/sh/create_pages.sh
-dos2unix .container/_scripts/sh/dump_lastmod.sh
-dos2unix .container/_scripts/sh/sync_monitor.sh
