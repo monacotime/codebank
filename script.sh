e8bc849049929dda81b0b1835eb2e1788f44a100
root <root@DESKTOP-35MGDJ3.localdomain>
2020-11-13 11:31:50 +0530

    [Automation] Initialize the environment.

---
+++ script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+dos2unix tools/build.sh  
+dos2unix tools/bump-version.sh  
+dos2unix tools/deploy.sh  init.sh  
+dos2unix tools/pv.sh  
+dos2unix tools/run.sh  
+dos2unix tools/test.sh
+dos2unix tools/init.sh
+dos2unix _scripts/sh/create_pages.sh  
+dos2unix _scripts/sh/dump_lastmod.sh  
+dos2unix _scripts/sh/sync_monitor.sh
+dos2unix .container/tools/build.sh 
+dos2unix .container/tools/bump-version.sh 
+dos2unix .container/tools/deploy.sh  init.sh 
+dos2unix .container/tools/pv.sh
+dos2unix .container/tools/run.sh  
+dos2unix .container/tools/test.sh
+dos2unix .container/tools/tools/init.sh
+dos2unix .container/_scripts/sh/create_pages.sh
+dos2unix .container/_scripts/sh/dump_lastmod.sh
+dos2unix .container/_scripts/sh/sync_monitor.sh
